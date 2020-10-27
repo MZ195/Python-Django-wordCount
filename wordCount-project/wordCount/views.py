b0fb50d02bdbd333ab4ce3b0da9dfd0a055eec38
Mazen.A <40984264+MZ195@users.noreply.github.com>
2020-10-27 18:55:58 +0300

    done

--- wordCount-project/wordCount/views.py
+++ wordCount-project/wordCount/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.http import request
 from django.shortcuts import render
+import operator
 
 
 def home(request):
@@ -8,4 +9,19 @@ def home(request):
 
 
 def count(request):
-    return render(request, "count.html")
+    fullText = request.GET['fulltext']
+
+    wordList = fullText.split()
+
+    word_dictionary = {}
+
+    for word in wordList:
+        if word in word_dictionary:
+            word_dictionary[word] += 1
+        else:
+            word_dictionary[word] = 1
+
+    sorted_word_dictionary = sorted(
+        word_dictionary.items(), key=operator.itemgetter(1), reverse=True)
+
+    return render(request, "count.html", {'fullText': fullText, 'count': len(wordList), 'wordDic': sorted_word_dictionary})
